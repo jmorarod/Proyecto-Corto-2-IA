84a32bd8844ee19002c2ec1440d71923ebdea9db
José Mora <josemorar96@gmail.com>
2018-05-22 01:12:02 -0600

    Implementada búsqueda A*

--- AStar.py
+++ AStar.py
@@ -1,15 +1,26 @@
 import math
-
+#Funcion principal, comentar
 def a_star_search(archivo, vision, zanahorias):
-    #try:
     tablero = leer_tablero(archivo)
-    print(tablero)
-    #while(zanahorias > 0):
-    costos = calculo_costo(tablero, vision, zanahorias)
-    print(costos)
-    #except:
-       # print("Error al leer el archivo de entrada")
+    if(tablero == []):
+        return
+    i = 0
+    #Implementar función revisar cantidad de zanahorias
+    while(zanahorias > 0):#or zanahorias en tablero = 0
+        #print_tablero(tablero)
+        costos = calculo_costo(tablero, vision, zanahorias)
+        movimiento = menor_costo(costos)
+        print("Paso: ",str(i).zfill(5))
+        tablero, zanahorias = mover_conejo(movimiento, tablero, zanahorias)
+        i += 1
+    print("Paso: ",str(i).zfill(5), "FINAL")
+    #print_tablero(tablero)
 
+#modificar función para imprimir a txt   
+def print_tablero(tablero):
+    for i in tablero:
+        print(i)
+#comentar
 def calculo_costo(tablero, rango, zanahorias_restantes):
     conejo = posicion_conejo(tablero)
     movimientos = movimientos_conejo(tablero, conejo)
@@ -17,7 +28,6 @@ def calculo_costo(tablero, rango, zanahorias_restantes):
     costos = []
     for casilla_movimiento in movimientos:
         costos_movimientos = []
-        
         for casilla_vision in vision_conejo:
             
             casilla = tablero[casilla_vision[0]][casilla_vision[1]]
@@ -37,20 +47,55 @@ def calculo_costo(tablero, rango, zanahorias_restantes):
                 costo_minimo = i
                 indice_minimo = indice
             indice += 1
-        costos += [casilla_movimiento[0], costos_movimientos[indice_minimo]]
+        try:
+            costos += [[casilla_movimiento[0], costos_movimientos[indice_minimo]]]
+        except:
+            costos += [[casilla_movimiento[0], 1000]]           
     return costos
-            
-
+#Comentar
+def mover_conejo(movimiento, tablero, zanahorias):
+    conejo = posicion_conejo(tablero)
+    x_conejo = conejo[0]
+    y_conejo = conejo[1]
+    tablero[x_conejo][y_conejo] = ' '
+    if(movimiento[0] == "DERECHA"):
+        if(tablero[x_conejo][y_conejo + 1] == 'Z'):
+            zanahorias -= 1
+        tablero[x_conejo][y_conejo + 1] = 'C'
+    elif(movimiento[0] == "IZQUIERDA"):
+        if(tablero[x_conejo][y_conejo - 1] == 'Z'):
+            zanahorias -= 1
+        tablero[x_conejo][y_conejo - 1] = 'C'
+    elif(movimiento[0] == "ARRIBA"):
+        if(tablero[x_conejo - 1][y_conejo] == 'Z'):
+            zanahorias -= 1
+        tablero[x_conejo - 1][y_conejo] = 'C'
+    elif(movimiento[0] == "ABAJO"):
+        if(tablero[x_conejo + 1][y_conejo] == 'Z'):
+            zanahorias -= 1
+        tablero[x_conejo + 1][y_conejo] = 'C'
+    return tablero, zanahorias
+        
+#Comentar
+def menor_costo(costos):
+    menor = float("inf")
+    min_costo = []
+    for costo in costos:
+        if(costo[1] < menor):
+            menor = costo[1]
+            min_costo = costo
+    return min_costo
+#Comentar
 def movimientos_conejo(tablero, conejo):
     movimientos = []
-    if(conejo[0] - 1 >= 0):
-        movimientos += [["IZQUIERDA",conejo[0] - 1, conejo[1]]]
-    if(conejo[0] + 1 < len(tablero[0])):
-        movimientos += [["DERECHA",conejo[0] + 1, conejo[1]]]
     if(conejo[1] - 1 >= 0):
-        movimientos += [["ARRIBA",conejo[0], conejo[1] - 1]]
-    if(conejo[1] + 1 < len(tablero)):
-        movimientos += [["ABAJO",conejo[0], conejo[1] + 1]]
+        movimientos += [["IZQUIERDA",conejo[0], conejo[1] - 1]]
+    if(conejo[1] + 1 < len(tablero[0]) - 1):
+        movimientos += [["DERECHA",conejo[0], conejo[1] + 1]]
+    if(conejo[0] - 1 >= 0):
+        movimientos += [["ARRIBA",conejo[0] - 1, conejo[1]]]
+    if(conejo[0] + 1 < len(tablero)):
+        movimientos += [["ABAJO",conejo[0] + 1, conejo[1]]]
     return movimientos
     
             
@@ -90,21 +135,24 @@ def cantidad_caracter_vecinos_por_casilla(caracter, casilla, tablero):
 #             y retorna una matriz con la estructura del problema
             
 def leer_tablero(archivo):
-    tablero = inicializar_tablero(archivo)
-    archivo = open(archivo,'r')
-    if(tablero == []):
-        return []
-    i = 0
-    j = 0
-    for line in archivo:
+    try:
+        tablero = inicializar_tablero(archivo)
+        archivo = open(archivo,'r')
+        if(tablero == []):
+            return []
+        i = 0
         j = 0
-        for char in line:
-            if(validar_caracter(char)):
-                tablero[i][j] = char
-            j += 1
-        i += 1
-    archivo.close()
-    return tablero
+        for line in archivo:
+            j = 0
+            for char in line:
+                if(validar_caracter(char)):
+                    tablero[i][j] = char
+                j += 1
+            i += 1
+        archivo.close()
+        return tablero
+    except:
+        return []
 
 #Entrada: Direccion de un archivo
 #Salida: Matriz de dimensiones N(filas del archivo) x
@@ -122,11 +170,8 @@ def inicializar_tablero(archivo):
         filas += 1
         columnas = 0
         for char in line:
-                columnas += 1
-        
-            
+                columnas += 1        
     tablero = [[0 for i in range(columnas)] for j in range(filas)]
-    print(tablero)
     return tablero
     
 
@@ -155,8 +200,8 @@ def rango_vision_conejo(tablero, posicion, rango):
     y_conejo = posicion[1]
     for i in range(x_conejo - rango, x_conejo + rango):
         for j in range(y_conejo - rango, y_conejo + rango):
-            if(i >= 0 and i < len(tablero) and j >= 0 and j < len(tablero[0])):
-                if(i != x_conejo and j != y_conejo):
+            if(i >= 0 and i < len(tablero) and j >= 0 and j < len(tablero[0]) - 1):
+                if(i != x_conejo or j != y_conejo):
                     vision += [[i,j]]
 
     return vision
